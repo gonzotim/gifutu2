920ffb0d10872fa4254d90cea1f3b126beb3a772
Tim Sutcliffe <tim@timsutcliffe.com>
2013-10-05 20:48:17 -0700

    moar 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,10 +77,12 @@ Gifutu2::Application.configure do
     config.action_mailer.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
       :port      => 587,
+      :domain    => 'gifutu2.herokuapp.com',
       :user_name => ENV["MANDRILL_USERNAME"],
-      :password  => ENV["MANDRILL_API_KEY"]
+      :password  => ENV["MANDRILL_API_KEY"],
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  
     }
-
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
