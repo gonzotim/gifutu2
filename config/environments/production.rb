de8fab20c705b4dcc832a1d3a4a3921613851503
Tim Sutcliffe <tim@timsutcliffe.com>
2013-10-05 19:25:06 -0700

    try to get mandril to work 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ Gifutu2::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify  
 
+  config.action_mailer.default_url_options = { :host => 'gifutu2.herokuapp.com' }
 
     config.action_mailer.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
